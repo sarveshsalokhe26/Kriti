bfe03ab8ae17a5f4e758d50e6c04cb33734660e6
sarveshsalokhe26 <sarveshsalokhe26@gmail.com>
2026-03-23 12:16:04 +0530

    chore(Dependencies): Adding multiple dependencies for frontend operations

--- kriti_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ kriti_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import firebase_core
+import firebase_messaging
+import flutter_secure_storage_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
